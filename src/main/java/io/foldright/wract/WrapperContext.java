0fd00a6deaebd017da3dda419d07e218e9b4c8a1
Jerry Lee <oldratlee@gmail.com>
2023-04-27 13:26:13 +0800

    initial commit

---
+++ src/main/java/io/foldright/wract/WrapperContext.java
@@ -0,0 +1,5 @@
+package io.foldright.wract;
+
+public interface WrapperContext<T> {
+    T unwrap();
+}
---
+++ src/test/java/io/foldright/wract/WrapperContextTest.java
@@ -0,0 +1,11 @@
+package io.foldright.wract;
+
+import org.junit.jupiter.api.Test;
+
+
+class WrapperContextTest {
+    @Test
+    void hello() {
+        // TODO
+    }
+}
