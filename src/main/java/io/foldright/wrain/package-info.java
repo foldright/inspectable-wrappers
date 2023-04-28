d9aef087ef98d3e32843e3a7c23975b180da498a
Jerry Lee <oldratlee@gmail.com>
2023-04-28 17:21:22 +0800

    docs: add `package-info.java` and `overview.html` 📚

---
+++ src/main/java/io/foldright/wrain/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * Wrain(Inspectable Wrapper Chain), a nano library provides the interfaces
+ * to make {@code wrapper instances} as an inspectable wrapper chain.
+ *
+ * <ul>
+ * <li>{@link io.foldright.wrain.Wrapper} is core interface, used to
+ *  enhance the wrapper instances as a wrapper chain;
+ *  and provides methods to inspect the wrapper chain.
+ * <li>{@link io.foldright.wrain.Attachable} interface is used to
+ * enhance the wrapper instances with the attachment storage ability.
+ * </ul>
+ *
+ * @see io.foldright.wrain.Wrapper
+ * @see io.foldright.wrain.Attachable
+ */
+package io.foldright.wrain;
