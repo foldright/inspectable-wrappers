c26f3658669c9fe1efbd7ad0e1aa8234940ffc28
Jerry Lee <oldratlee@gmail.com>
2023-04-28 16:14:03 +0800

    test: refactor `WrapperTest` 🐒

--- src/test/java/io/foldright/wrain/WrapperTest.kt
+++ src/test/java/io/foldright/wrain/WrapperTest.kt
@@ -1,26 +1,21 @@
 package io.foldright.wrain
 
-import io.foldright.demo.ChattyExecutorWrapper
-import io.foldright.demo.LazyExecutorWrapper
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
 import java.util.concurrent.Executor
 import java.util.concurrent.ExecutorService
 
 class WrapperTest : FunSpec({
 
-    ////////////////////////////////////////
     // prepare executor instance and wrappers
-    ////////////////////////////////////////
 
-    val executor = Runnable::run
-
-    val lazy = LazyExecutorWrapper(executor)
-    lazy.wrainSet("busy", "very very busy!")
-
+    val executor = Executor { it.run() }
+    val lazy: Executor = LazyExecutorWrapper(executor).apply { wrainSet("busy", "very very busy!") }
     val chatty: Executor = ChattyExecutorWrapper(lazy)
 
     test("wrapper") {
@@ -33,3 +28,41 @@ class WrapperTest : FunSpec({
         Wrapper.getAttachment<Executor, String>(chatty, "not existed").shouldBeNull()
     }
 })
+
+class ChattyExecutorWrapper(private val executor: Executor) : Executor, Wrapper<Executor> {
+    override fun execute(command: Runnable) {
+        println("BlaBlaBla...")
+        executor.execute(command)
+    }
+
+    override fun wrainUnwrap(): Executor {
+        return executor
+    }
+}
+
+class LazyExecutorWrapper(private val executor: Executor) : Executor, Wrapper<Executor>, Attachable {
+    override fun execute(command: Runnable) {
+        println("I'm lazy, sleep before work")
+        sleep()
+        executor.execute(command)
+    }
+
+    override fun wrainUnwrap(): Executor {
+        return executor
+    }
+
+    private val attachments: ConcurrentMap<String, Any> = ConcurrentHashMap()
+
+    override fun wrainSet(key: String, value: Any) {
+        attachments[key] = value
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <V> wrainGet(key: String): V {
+        return attachments[key] as V
+    }
+
+    private fun sleep() {
+        Thread.sleep(100)
+    }
+}
