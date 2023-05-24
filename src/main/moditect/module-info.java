5f46ce8aff640cf1350ea77eec2c638b4aeb531d
Jerry Lee <oldratlee@gmail.com>
2023-05-24 19:28:51 +0800

    feat: support `java9 jigsaw` 📦 , add `module-info.java` file

---
+++ src/main/moditect/module-info.java
@@ -0,0 +1,6 @@
+// https://www.oracle.com/corporate/features/understanding-java-9-modules.html
+
+module io.foldright.cffu.ttl.executor.wrapper {
+    exports io.foldright.cffu;
+    exports io.foldright.cffu.utils;
+}
