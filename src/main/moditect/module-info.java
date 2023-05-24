4c3053487fa366c5bd4f957e7015e84f200ddc6c
Jerry Lee <oldratlee@gmail.com>
2023-05-24 19:28:51 +0800

    feat: support `java9 jigsaw` 📦 , add `module-info.java` file

---
+++ src/main/moditect/module-info.java
@@ -0,0 +1,6 @@
+// https://www.oracle.com/corporate/features/understanding-java-9-modules.html
+
+module io.foldright.inspectablewrappers {
+    exports io.foldright.inspectablewrappers;
+    exports io.foldright.inspectablewrappers.utils;
+}
