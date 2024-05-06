45585b1c05a11336527ba60c3f939cf98cacc3a7
Jerry Lee <oldratlee@gmail.com>
2024-05-07 02:27:18 +0800

    WIP docs the concepts and design pattern of inspectablewrappers

--- src/main/java/io/foldright/inspectablewrappers/package-info.java
+++ src/main/java/io/foldright/inspectablewrappers/package-info.java
@@ -14,6 +14,29 @@
  * <li>The {@link io.foldright.inspectablewrappers.Inspector} class is used to inspect the wrapper chain.
  * </ul>
  *
+ * <h2>About wrapper pattern</h2>
+ *
+ * <a href="https://refactoring.guru/design-patterns/decorator">Wrapper pattern(aka. Decorator pattern)</a> is
+ * well-known and widely-used, is used to attach new behaviors to objects
+ * (the <strong>wrappee</strong>/<strong>underlying instances</strong>/<strong>wrapped instances</strong>)
+ * by placing these objects inside the <strong>wrapper</strong> objects that contain the behaviors.
+ * <p>
+ * Well-known examples of wrapper pattern in java std lib:
+ * <ul>
+ * <li>Collection Wrappers:<sup><a href="https://docs.oracle.com/javase/tutorial/collections/implementations/wrapper.html">
+ *     (Wrapper Implementations - The Java™ Tutorials)</a></sup>
+ *   <ul>
+ *   <li>Unmodifiable Wrappers, e.g.
+ *     {@link java.util.Collections#unmodifiableList(java.util.List) Collections#unmodifiableList(List)}
+ *   <li>Synchronization Wrappers, e.g.
+ *     {@link java.util.Collections#synchronizedCollection(java.util.Collection) Collections#synchronizedCollection(Collection)}
+ *   </ul>
+ * <li>{@link java.io.BufferedReader#BufferedReader(java.io.Reader) BufferedReader#BufferedReader(Reader)}
+ * </ul>
+ * <p>
+ * The related design patterns are <a href="https://refactoring.guru/design-patterns/adapter">Adapter </a>
+ * the wrapper instances and wra
+ *
  * @author Jerry Lee (oldratlee at gmail dot com)
  * @author Zava Xu (zava dot kid at gmail dot com)
  * @author Yang Fang (snoop dot fy at gmail dot com)
